152afe183a9b3d2a86391ed69593de3489daeed3
thomas aquino <50937655+thomasaquino73@users.noreply.github.com>
2026-05-13 19:38:37 +0700

    ss

--- resources/views/partials/pengaturan/navbar_pengaturan.blade.php
+++ resources/views/partials/pengaturan/navbar_pengaturan.blade.php
@@ -1,20 +1,15 @@
 <ul class="nav nav-pills flex-column flex-md-row mb-4">
 
-    @if (auth()->user()->can('application_system-browse'))
-        <li class="nav-item">
-            <a class="nav-link {{ request()->routeIs('pengaturan.sistem') ? 'active' : '' }}"
-                href="{{ url('pengaturan-sistem') }}">
-                <i class="ti ti-database ti-xs me-1"></i>System
-            </a>
-        </li>
-    @endif
-    @if (auth()->user()->can('login_background-browse'))
-        <li class="nav-item">
-            <a class="nav-link {{ request()->routeIs('pengaturan.background.*') ? 'active' : '' }}"
-                href="{{ route('pengaturan.background.index') }}">
-                <i class="ti ti-layout-board ti-xs me-1"></i>Login Background
-            </a>
-        </li>
-    @endif
-
+    <li class="nav-item">
+        <a class="nav-link {{ request()->routeIs('pengaturan.sistem') ? 'active' : '' }}"
+            href="{{ url('pengaturan-sistem') }}">
+            <i class="ti ti-database ti-xs me-1"></i>System
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link {{ request()->routeIs('pengaturan.background.*') ? 'active' : '' }}"
+            href="{{ route('pengaturan.background.index') }}">
+            <i class="ti ti-layout-board ti-xs me-1"></i>Login Background
+        </a>
+    </li>
 </ul>
